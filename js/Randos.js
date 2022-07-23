bf3afaa3737f3b68d722281838d2db2be72f7aaf
AntoineMdn <antoine.mandin.am@gmail.com>
2022-07-23 12:49:52 +0200

    Introduce reading randos

---
+++ js/Randos.js
@@ -0,0 +1,30 @@
+export class GeoShape {
+    constructor (coordinates=[]) {
+        this.coordinates = coordinates
+    }
+}
+
+export class Rando {
+    constructor (args={}) {
+        this.length_linear = args.fields.lineaire
+        this.geo_point = args.fields.geo_point_2d
+        this.category = args.fields.cat
+        this.type_ffrp = args.fields.type_ffrp
+        this.commune = args.fields.commune
+        this.difficulte = args.fields.difficulte
+        this.published = args.fields.published
+        this.geo_shape = new GeoShape(args.fields.geo_point_2d.coordinates)
+    }
+}
+
+export async function loadRandos() {
+    let response = await fetch("../data/randos.json")
+    let data = await response.json()
+    let result = []
+    for (let rawRando of data) {
+        result.push(new Rando(rawRando))
+    }
+    return result
+}
+
+export default { GeoShape, Rando }
--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 import { Map } from "./Map.js";
-
+import { loadRandos } from "./Randos.js"
 
 function buildMap() {
     let map = new Map()
@@ -7,6 +7,14 @@ function buildMap() {
 
 function main() {
     buildMap()
+    loadRandos()
+        .then(randos => {
+            console.log(randos.length)
+            console.log(randos)
+        })
+        .catch(err => {
+            console.warn("Error loading randos", err)
+        })
 }
 
 main()
